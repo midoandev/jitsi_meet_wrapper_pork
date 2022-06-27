171a2624cdcf002158518ee95e64ff98e0377c7b
Mido <43594864+midoandev@users.noreply.github.com>
2022-06-27 12:56:54 +0700

    remove construct

--- jitsi_meet_wrapper/android/src/main/kotlin/dev/saibotma/jitsi_meet_wrapper/JitsiMeetWrapperPlugin.kt
+++ jitsi_meet_wrapper/android/src/main/kotlin/dev/saibotma/jitsi_meet_wrapper/JitsiMeetWrapperPlugin.kt
@@ -28,11 +28,6 @@ class JitsiMeetWrapperPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     
     private var activity: Activity? = null
     
-    
-    constructor(activity: Activity?) : this() {
-        this.activity = activity
-    }
-    
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "jitsi_meet_wrapper")
         methodChannel.setMethodCallHandler(this)
